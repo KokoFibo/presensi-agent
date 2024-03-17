67ebd34a8d9794c2c07f61e0898d0d6590f0d66b
KokoFibo <kokonacci@gmail.com>
2024-03-17 12:13:40 +0700

    show camera 7

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -69,38 +69,34 @@
         }
 
         function startScan() {
-            let cameraId;
-
-            Html5Qrcode.getCameras().then(devices => {
-                /**
-                 * devices would be an array of objects of type:
-                 * { id: "id", label: "label" }
-                 */
-                if (devices && devices.length) {
-                    cameraId = devices[0].id;
-                    // Initialize Html5Qrcode after getting cameraId
-                    const html5QrCode = new Html5Qrcode("reader");
-                    html5QrCode.start(
-                            cameraId, {
-                                fps: 10, // Optional, frame per seconds for qr code scanning
-                                qrbox: {
-                                    width: 250,
-                                    height: 250
-                                } // Optional, if you want bounded box UI
-                            },
-                            (decodedText, decodedResult) => {
-                                // do something when code is read
-                            },
-                            (errorMessage) => {
-                                // parse error, ignore it.
-                            })
-                        .catch((err) => {
-                            // Start failed, handle it.
-                        });
+            const html5QrCode = new Html5Qrcode("reader");
+            const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+                /* handle success */
+            };
+            const config = {
+                fps: 10,
+                qrbox: {
+                    width: 250,
+                    height: 250
                 }
-            }).catch(err => {
-                // handle err
-            });
+            };
+
+            // If you want to prefer front camera
+            // html5QrCode.start({
+            //     facingMode: "user"
+            // }, config, qrCodeSuccessCallback);
+
+            // If you want to prefer back camera
+            html5QrCode.start({
+                facingMode: "environment"
+            }, config, qrCodeSuccessCallback);
+
+            // Select front camera or fail with `OverconstrainedError`.
+            // html5QrCode.start({ facingMode: { exact: "user"} }, config, qrCodeSuccessCallback);
+
+            // Select back camera or fail with `OverconstrainedError`.
+            // html5QrCode.start({ facingMode: { exact: "environment"} }, config, qrCodeSuccessCallback);
+
         }
     </script>
 @endsection
